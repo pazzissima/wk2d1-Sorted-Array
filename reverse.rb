ebc94a143007bf070d17673e195b162a13845a85
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2014-01-13 21:59:19 -0800

    Delete reverse.rb

--- reverse.rb
+++
@@ -1,9 +0,0 @@
-# Saving my friends name
-puts "Hello, what is your name?"
-
-YourName = gets.chomp
-
-#Saving the number of completed assignments
-puts "How many prework assignments have you completed?"
-Prework = gets.chomp
-puts "Hello, " + YourName + " I'm Tim. Nice to meet you. I see you have completed " + Prework + " assignments. I have completed 1 assignment."
